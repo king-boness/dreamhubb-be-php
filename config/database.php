9133f5de9164b5b53141615f20472de18248a3df
Matej Kostun <matej.kostun@gmail.com>
2025-10-24 23:11:04 +0200

    fix: stable internal DB connection for Render

--- config/database.php
+++ config/database.php
@@ -59,8 +59,8 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-                'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
