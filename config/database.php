29eef79ad835b66d9c153016e7189d7a3a4febb4
Matej Kostun <matej.kostun@gmail.com>
2025-10-24 22:40:18 +0200

    fix: render pgsql ssl prefer mode

--- config/database.php
+++ config/database.php
@@ -60,18 +60,19 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'dpg-dt3q5v5r7b73flue90-a.oregon-postgres.render.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'dreamhubb_db_bhg3'),
-            'username' => env('DB_USERNAME', 'dreamhubb_db_bhg3_user'),
-            'password' => env('DB_PASSWORD', 'TVOJE_HESLO_Z_RENDERU'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer', // 🔥 najdôležitejšie
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
             'options' => extension_loaded('pdo_pgsql') ? array_filter([
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::PGSQL_ATTR_SSL_MODE => env('DB_SSLMODE', 'prefer'),
             ]) : [],
         ],
 
