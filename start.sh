5bc2aaefe097846b0fbf1a0b7abe47434c2bf87d
Matej Kostun <matej.kostun@gmail.com>
2025-10-24 17:42:29 +0200

    fix: add permissions and env for Laravel runtime

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-# Inštalácia Composeru
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
-# Inštalácia PHP závislostí
 composer install --no-dev --optimize-autoloader
 
 php artisan key:generate || true
 php artisan config:clear || true
 php artisan cache:clear || true
 php artisan migrate --force || true
+
+chmod -R 775 storage bootstrap/cache
+chown -R root:root storage bootstrap/cache
 
 php artisan serve --host=0.0.0.0 --port=10000
