e0a237e194b1056a9921673c2e1612385e889255
Matej Kostun <matej.kostun@gmail.com>
2025-10-25 17:32:41 +0200

    Remove PDO SSL constants for Render compatibility

--- config/database.php
+++ config/database.php
@@ -68,11 +68,12 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'sslmode' => env('DB_SSLMODE', 'require'),
-            'options' => extension_loaded('pdo_pgsql') ? array_filter([
-                PDO::PGSQL_ATTR_SSL_MODE => 'require',
-                PDO::PGSQL_ATTR_SSL_ROOT_CERT => '/etc/ssl/certs/ca-certificates.crt',
-            ]) : [],
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+
+            // 💡 Dôležité: žiadne PDO::PGSQL_ATTR_* konštanty
+            'options' => extension_loaded('pdo_pgsql') ? [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ] : [],
         ],
 
         'sqlsrv' => [
